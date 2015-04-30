a91a34c12df076782c42688fa882c49804b3aed6
derekkuo0727 <derekkuo@ischool.berkeley.edu>
2015-04-29 19:49:44 -0700

    firstTest

--- LinkedLiving/tests.py
+++ LinkedLiving/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
+from LinkedLiving.views import GetActivityView
 
 class SimpleTest(TestCase):
-	def test_api(self):
-		self.assertTrue(True)
+	#Test if get_trend api function
+	def test_api_get_daily_view(self):
+		jsonResponse = GetActivityView.get("http://127.0.0.1:8000/api/get_trend/?user_id=123&start_datetime=1427871600")
+		self.assertEqual("{\"table\": []}",jsonResponse)
+
