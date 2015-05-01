8cecbd5dacde8d00ba8b08d2cdb6865f54d6cc90
derekkuo0727 <derekkuo@ischool.berkeley.edu>
2015-04-30 18:28:26 -0700

    build#1

--- LinkedLiving/tests.py
+++ LinkedLiving/tests.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 from LinkedLiving.views import GetDailyView
 
 class SimpleTest(TestCase):
-	#Test if get_daily api function
-	def test_api_get_daily_view(self):
+	#Test if get_daily api function working correctly
+	def test_api_get_daily_view_wrong_range(self):
 		daily = GetDailyView()
 		response = daily.returnData(-1,-1)
 		self.assertEqual([],response)
