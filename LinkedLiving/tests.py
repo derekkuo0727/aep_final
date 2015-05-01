24e4601bfb2b0525b2afd94395b2ef0e8115ddb2
derekkuo0727 <derekkuo@ischool.berkeley.edu>
2015-04-30 21:01:53 -0700

    build#2

--- LinkedLiving/tests.py
+++ LinkedLiving/tests.py
@@ -2,9 +2,14 @@ from django.test import TestCase
 from LinkedLiving.views import GetDailyView
 
 class SimpleTest(TestCase):
-	#Test if get_daily api function working correctly
-	def test_api_get_daily_view_wrong_range(self):
+	#Test if get_daily api function return an empty list while input range is wrong
+	def test_api_get_daily_view_input_wrong_range(self):
 		daily = GetDailyView()
 		response = daily.returnData(-1,-1)
 		self.assertEqual([],response)
+	#Test if get_daily api function return the correct json file while input range is correct
+	def test_api_get_daily_view_input_correct_range(self):
+		daily = GetDailyView()
+		response = daily.returnData(1427871600,1427957999)
+		self.assertEqual(758,len(response))
 
